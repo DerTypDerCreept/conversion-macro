d0626dd3ba8ff18fc3cf104fcf5a364ea69c7533
Bjoern Mueller <Bjoern.Mueller.Info@web.de>
2014-06-18 08:07:03 +0200

    Please just upload

--- core/src/main/scala/Test.scala
+++ core/src/main/scala/Test.scala
@@ -1,5 +1,6 @@
 import TestTypes._
 import language.implicitConversions
+import language.higherKinds
 import org.scalatest._
 
 object Helper{
@@ -10,124 +11,46 @@ object Helper{
     }
     //standard test seperator
     def spts(text: String) = pts(text,"=",75)
+    
 }
 
-object Test extends App with ConvertMe with Expr{
+object Test extends App with ConvertMe with ConvertMe2{
     import Helper.spts
+	
+	
+        
     //import ConvertMe._
     spts("hello")
     //ConvertMe.saySomething
-    val xs = Cons(1, Cons(2, Cons(3, Cons(4, Nil()))))
-      def sum1(xs: Lists[Int]): Int = xs.fold[Int] {
-        case Nil() => 0
-        case Cons(head, tail) => head + tail
-      }
-      def sum2(xs: Lists[Int]): Int = xs match {
-        case Nil() => 0
-        case Cons(head, tail) => head + sum2(tail)
-      }
-      println(s"sum1 = ${sum1(xs)}")
-      println(s"sum2 = ${sum2(xs)}")
-	  val value = Mul(Add(Num(5),Num(5)),Num(2))
-	  val value2 = Mul(Add(Num(5),Num(5)),Num(3))
-	  val value3 = Add(Add(Num(5),Num(5)),Num(2))
-	  val value4 = Mul(Add(Num(5),Num(5)),Num(2))
-	  // /*
-	  assert(!value.equals(null))
-	  assert(value.equals(value))
-	  assert(value.equals(value4))
-	  assert(!value.equals(value2))
-	  assert(!value.equals(value3))
-	  assert(!value.equals("No Way"))
-	  println("equals assertions went through")
-	  println(value)
-	  // */
-	  /*
-	  val n = Num(5)
-	  val m = Num.unapply(n)
-	  println(m)
-	  println(value)
-	  
-	  val p = n match {
-		case Num(n) => n+1
-	  }
-	  println(p)
-	  
-	  def eval(e:Exp):Int = e match{
-		case Num(a) => a
-		case Add(a,b) => eval(a)+eval(b)
-		case Mul(a,b) => eval(a)*eval(b)
-		
-	  }
-	  
-	  println(eval(value))
-	  */
-	  def foldExp(e: Exp) : Int = e.fold[Int]{
-		case Num(n) => n
-		//case Id(x) => x
-		case Add(lhs,rhs) => lhs + rhs
-		case Mul(lhs,rhs) => lhs * rhs
-	  }
-	  
-	  println(s"exp1 = ${foldExp(value)}")
-	
-	}
+    val tree: Tree =
+    Branch(List(
+      Branch(List(
+        Leaf(1),
+        Leaf(2))),
+      Leaf(3),
+      Branch(List(
+        Leaf(4),
+        Leaf(5)))))
 
-    	
-
-// /*
-trait DataTypes
-extends Companies
-   with BinTrees
-///*
-class BinTreeSanityTest extends FlatSpec with DataTypes {
-  "completeBinaryTree" should "produce complete binary trees" in {
-    val List(bin1, bin2, bin3, bin4, bin5, bin6, bin7, bin8, bin9) =
-      (1 to 9).map(completeBinTree).toList
-    implicit def convert(n: Int): BinTree[Int] = Leaf(n)
-    assert(bin1 == Leaf(1))
-    assert(bin2 == Bin(1, 2))
-    assert(bin3 == Bin(Bin(1, 2), 3))
-    assert(bin4 == Bin(Bin(1, 2), Bin(3, 4)))
-    assert(bin5 == Bin(Bin(Bin(1, 2), 3), Bin(4, 5)))
-    assert(bin6 == Bin(Bin(Bin(1, 2), Bin(3, 4)), Bin(5, 6)))
-    assert(bin7 == Bin(Bin(Bin(1, 2), Bin(3, 4)), Bin(Bin(5, 6), 7)))
-    assert(bin8 == Bin(Bin(Bin(1, 2), Bin(3, 4)), Bin(Bin(5, 6), Bin(7, 8))))
-    assert(bin9 == Bin(Bin(Bin(Bin(1, 2), 3), Bin(4, 5)), Bin(Bin(6, 7), Bin(8, 9))))
+    val tree2: Tree2 =
+    Branch2(Set(
+      Branch2(Set(
+        Leaf2(1),
+        Leaf2(2))),
+      Leaf2(3),
+      Branch2(Set(
+        Leaf2(4),
+        Leaf2(5)))))    
+        
+  def sumLeaves(t: Tree): Int = t.fold[Int] {
+    case LeafF(n) => n
+    case BranchF(ns) => ns.sum
   }
-}
-// */
-// this example is for correctness.
-// @converMe macro doesn't make it easier to write
-// due to not supporting mutually recursive data types.
-//*
-class SelectSalary extends FlatSpec with DataTypes {
-
-  // collects all salary nodes in a list
-  def selectSalary(company: Company): List[Salary] = {
-    def selectSalary(department: Department): List[Salary] =
-      department.manager.salary :: (
-        for {
-          unit <- department.dUnits
-          salary = unit match {
-            case PU(employee) => List(employee.salary)
-            case DU(department) => selectSalary(department)
-          }
-          _ <- NoReturn
-        }
-        yield salary
-      )
-    company.departments flatMap selectSalary
+  def sumLeaves2(t: Tree2): Int = t.fold[Int] {
+    case Leaf2F(n) => n
+    case Branch2F(ns) => ns.sum
   }
-
-  "selectSalary" should "collect salary nodes in some order" in {
-    val sortedSalaries = selectSalary(genCom).sortBy(_.amount)
-    assert(sortedSalaries == List[Float](1000, 2000, 8000, 100000).map(Salary.apply))
-  }
+	println(tree2)
+  println(s"sumLeaves( [[1, 2], 3, [4, 5]] ) = ${sumLeaves(tree)}")
+  println(s"sumLeaves( [[1, 2], 3, [4, 5]] ) = ${sumLeaves2(tree2)}")
 }
-// */
-object NoReturn {
-  def map[T](f: Unit => T): T = f(())
-}
-
-// */
--- core/src/main/scala/TestTypes.scala
+++ core/src/main/scala/TestTypes.scala
@@ -1,105 +1,32 @@
+import language.higherKinds
 object TestTypes{
+
+	trait Functor[F[_]] {
+        def fmap[A, B](f: A => B)(fa: F[A]): F[B]
+    }
+
+        implicit def listFunctor: Functor[List] = new Functor[List] {
+                def fmap[A, B](f: A => B)(fa: List[A]): List[B] = fa map f
+            }
+        implicit def setFunctor: Functor[Set] = new Functor[Set] {
+                def fmap[A, B](f: A => B)(fa: Set[A]): Set[B] = fa map f
+            }
+    
 
 @convert
 trait ConvertMe{
-  //fixed Point
-  trait Lists[T]
+  trait Tree
+  
+  case class Leaf(tag:Int) extends Tree
+  case class Branch(children:List[Tree]) extends Tree
 
-  //variants	
-  case class Nil[T] extends Lists[T]
-  case class Cons[T](head:T, tail:Lists[T]) extends Lists[T]
 }
- /*
 @convert
-trait Expr{
-	trait Exp
-	case class Num(n: Int) extends Exp
-	case class Add(lhs: Exp, rhs: Exp) extends Exp
-	case class Mul(lhs: Exp, rhs: Exp) extends Exp
-	case class Id(x: Symbol) extends Exp 
-}
-// */
-// /*
-@convert
-trait FixPoint{
-	trait Exp
-}
-
-@convert
-trait FixNumber extends FixPoint{
-	case class Num(n: Int) extends Exp
-}
-@convert 
-trait Expr extends FixNumber{
-	case class Add(lhs: Exp, rhs: Exp) extends Exp
-	case class Mul(lhs: Exp, rhs: Exp) extends Exp
-	case class Id(x: Symbol) extends Exp 
-}
-// */
-// paradise benchmark in "scrap your boilerplate"
-@convert
-trait Companies {
-  case class Company(departments: List[Department])
-  case class Department(name: Name, manager: Manager, dUnits: List[DUnit])
-  trait DUnit
-  case class Employee(person: Person, salary: Salary)
-  case class PU(employee: Employee) extends DUnit
-  case class DU(department: Department) extends DUnit
-  case class Person(name: Name, address: Address)
-  case class Salary(amount: Float)
-  type Manager = Employee
-  type Name = String
-  type Address = String
-
-  val ralf = Employee(Person("Ralf", "Amsterdam"), Salary( 8000))
-  val joost = Employee(Person("Joost", "Amsterdam"), Salary( 1000))
-  val marlow = Employee(Person("Marlow", "Cambridge"), Salary( 2000))
-  val blair = Employee(Person("Blair", "London" ), Salary(100000))
-
-  // facepalm: if genCom is declared before ralf, jost, marlow & blair then
-  // expect NullPointerException.
-  val genCom: Company =
-    Company(List(
-      Department("Research", ralf, List(PU(joost), PU(marlow))),
-      Department("Strategy", blair, Nil)))
-}
-
-// binary trees with arbitrary uniform data at leaves
-@convert
-trait BinTrees {
-  trait BinTree[A]
-  case class Leaf[A](get: A) extends BinTree[A]
-  case class Bin[A](leftChild: BinTree[A], rightChild: BinTree[A]) extends BinTree[A]
-
-  // the complete binary tree with n leaves labelled 1 to n
-  def completeBinTree(n: Int): BinTree[Int] = {
-    def log2(n: Int): Int = 31 - java.lang.Integer.numberOfLeadingZeros(n)
-
-    // generate complete binary tree with leaves labelled from to (to - 1)
-    def loop(from: Int, to: Int): BinTree[Int] =
-      if (to - from == 1)
-        Leaf(from)
-      else {
-        val n = to - from
-        val mostSignificantBit = log2(n)
-        val pow = 1 << mostSignificantBit
-        val halfPow = pow / 2
-        //val right = pow / 2 // the largest power of 2 not exceeding n/2
-        val mid =
-          // case rhs is power of 2
-          if (0 == (n & halfPow)) to - halfPow
-          // case lhs is power of 2
-          // subcase perfect binary tree
-          else if (n == pow)
-            from + halfPow
-          // subcase imperfect binary tree
-          else
-            from + pow
-        Bin(loop(from, mid), loop(mid, to))
-      }
-
-    loop(1, n + 1)
-  }
+trait ConvertMe2{
+      trait Tree2
+  case class Leaf2(tag:Int) extends Tree2
+  case class Branch2(children:Set[Tree2]) extends Tree2 
+  
 }
 
 }
